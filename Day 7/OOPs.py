95b91848b1df4fee42d0d5214f5047074482f56b
itsmohil <mohil1mvp@gmail.com>
2022-10-06 17:43:19 +0100

    OOPs updated

---
+++ Day 7/OOPs.py
@@ -0,0 +1,21 @@
+# Example 1:
+# class myClass:
+#     def myFun(self):
+#         pass
+#     def display(self,name):
+#         print(name)
+#
+#
+# mc = myClass()
+#
+# # mc.myFun()
+# mc.display("Mohil")  # John
+
+# Example 2
+class myClass:
+    def m1(self):
+        print("This is instance method...")
+    @staticmethod
+        def m2(self,num):
+        print(num)
+
