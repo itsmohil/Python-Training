f783ae1e52535f85749ce978faea186368c8ffe4
itsmohil <mohil1mvp@gmail.com>
2022-10-17 09:32:40 +0100

    First test updated

--- Day 1/FirstTestCase.py
+++ Day 1/FirstTestCase.py
@@ -16,10 +16,10 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 serv_obj = Service("C:\Project Drivers\chromedriver_win32\chromedriver.exe")
 driver = webdriver.Chrome(service=serv_obj)
-#driver = webdriver.Chrome()
+driver = webdriver.Chrome()
 driver.get("https://opensource-demo.orangehrmlive.com/")
 driver.find_element(By.NAME,"txtUsername").send_keys("Admin123")
-driver.find_element(By.ID,"txtPassword").send_keys("dmin123")
+driver.find_element(By.ID,"txtPassword").send_keys("Admin123")
 driver.find_element(By.ID,"btnLogin").click()
 act_title = driver.title
 exp_title = "OrangeHRM"
